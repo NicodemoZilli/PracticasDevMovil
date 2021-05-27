b3abc607edbc556214d136f50bc9033cb7181d0e
Nicodemo Zilli <nicozv13@gmail.com>
2021-05-27 00:50:31 -0500

    actualizado

---
+++ EnviaSMS/app/src/main/java/com/dnzv/enviasms/basededatos/enviasms/n17011289/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dnzv.enviasms.basededatos.enviasms.n17011289;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
